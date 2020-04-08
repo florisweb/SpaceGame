152e5b9c92d5a58c7c67b6b5522e47408987f829
florisweb <github@florisweb.tk>
2020-04-08 10:18:51 +0200

    Added a basic drag-system

--- js/inputHandler.js
+++ js/inputHandler.js
@@ -1,5 +1,48 @@
+document.onmousedown = function() { 
+  InputHandler.mouseDown = true;
+}
+document.onmouseup = function() {
+  InputHandler.mouseDown = false;
+}
+
+
 function _InputHandler() {
 	let HTML = {
 		canvas: gameCanvas,
 	}
+	this.mouseDown = false;
+	this.draging = false;
+
+
+
+	HTML.canvas.addEventListener("mousedown", 
+    	function (_event) {
+      		InputHandler.draging = true;
+    	}
+  	);
+
+  	HTML.canvas.addEventListener("mouseup", stopDraging);
+
+  	let prevDragVector = false;
+	HTML.canvas.addEventListener("mousemove", 
+	    function (_event) {
+	    	if (!InputHandler.draging) return;
+	    	if (!InputHandler.mouseDown) return stopDraging();
+
+	    	if (prevDragVector)
+	    	{
+	    		let deltaPos = new Vector([_event.screenX, _event.screenY]).difference(prevDragVector);
+	    		RenderEngine.camera.position.add(deltaPos);
+	    	}
+
+	    	prevDragVector = new Vector([_event.screenX, _event.screenY]);
+	    }
+	);
+	
+
+	function stopDraging() {
+		InputHandler.draging = false;
+      	prevDrag = false;
+	}
+
 }
